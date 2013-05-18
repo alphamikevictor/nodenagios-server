3969d92ffbff7c87a9e0d89047c3157357748942
Agustin <alphamikevictor@gmail.com>
2013-05-18 11:43:00 +0200

    Dumb commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var configuration = {
     port: 8080,
     phrase: 'IWantM0nitorYo4',
-    pluginsdir: '/var/lib/stickshift/51961e365973ca9233000ddc/app-root/data/501287/plugins',
+    pluginsdir: '/Users/agustin/projects:nodenagios-server/plugins',
     sysinternalsdir: 'c:\\tools\\sysinternals\\'
 }
 
